florentx/odoo

[FIX] do not set the category of modules at initialization. Wait the registry to be available to allow tracking of generated xmlids [IMP] create_categories(): avoid a useless query as we already know the xmlids

openerp/modules/db.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2012 OpenERP s.a. (<http://openerp.com>).
+#    Copyright (C) 2010-2013 OpenERP s.a. (<http://openerp.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -64,14 +64,16 @@ def initialize(cr):
 
         if not info:
             continue
-        categories = info['category'].split('/')
-        category_id = create_categories(cr, categories)
+
+        # NOTE: category will be set later when registry will be available
+        category_id = None
 
         if info['installable']:
             state = 'uninstalled'
         else:
             state = 'uninstallable'
 
+        # TODO insert minimal module (only name, state, auto_install)
         cr.execute('INSERT INTO ir_module_module \
                 (author, website, name, shortdesc, description, \
                     category_id, auto_install, state, web, license, application, icon, sequence, summary) \
@@ -115,6 +117,7 @@ def create_categories(cr, categories):
     Return the database id of the (last) category.
 
     """
+    registry = openerp.modules.registry.RegistryManager.registries[cr.dbname]
     p_id = None
     category = []
     while categories:
@@ -134,8 +137,12 @@ def create_categories(cr, categories):
                        VALUES (%s, %s, %s, %s)', ('base', xml_id, c_id, 'ir.module.category'))
         else:
             c_id = c_id[0]
+
+        registry.model_data_reference_ids[('base', xml_id)] = ('ir.module.category', c_id)
+
         p_id = c_id
         categories = categories[1:]
+
     return p_id
 
 def has_unaccent(cr):

openerp/modules/loading.py
@@ -264,6 +264,7 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
     try:
         if not openerp.modules.db.is_initialized(cr):
             _logger.info("init db")
+            update_module = True
             openerp.modules.db.initialize(cr)
             tools.config["init"]["all"] = 1
             tools.config['update']['all'] = 1
@@ -296,7 +297,8 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
         # STEP 2: Mark other modules to be loaded/updated
         if update_module:
             modobj = registry['ir.module.module']
-            if ('base' in tools.config['init']) or ('base' in tools.config['update']):
+            iu = tools.config['init'].keys() + tools.config['update'].keys()
+            if ('base' in iu) or ('all' in iu):
                 _logger.info('updating modules list')
                 modobj.update_list(cr, SUPERUSER_ID)
 

Odew/odoo-1

openerp/modules/db.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2012 OpenERP s.a. (<http://openerp.com>).
+#    Copyright (C) 2010-2013 OpenERP s.a. (<http://openerp.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -64,14 +64,16 @@ def initialize(cr):
 
         if not info:
             continue
-        categories = info['category'].split('/')
-        category_id = create_categories(cr, categories)
+
+        # NOTE: category will be set later when registry will be available
+        category_id = None
 
         if info['installable']:
             state = 'uninstalled'
         else:
             state = 'uninstallable'
 
+        # TODO insert minimal module (only name, state, auto_install)
         cr.execute('INSERT INTO ir_module_module \
                 (author, website, name, shortdesc, description, \
                     category_id, auto_install, state, web, license, application, icon, sequence, summary) \
@@ -115,6 +117,7 @@ def create_categories(cr, categories):
     Return the database id of the (last) category.
 
     """
+    registry = openerp.modules.registry.RegistryManager.registries[cr.dbname]
     p_id = None
     category = []
     while categories:
@@ -134,8 +137,12 @@ def create_categories(cr, categories):
                        VALUES (%s, %s, %s, %s)', ('base', xml_id, c_id, 'ir.module.category'))
         else:
             c_id = c_id[0]
+
+        registry._model_data_reference_ids[('base', xml_id)] = ('ir.module.category', c_id)
+
         p_id = c_id
         categories = categories[1:]
+
     return p_id
 
 def has_unaccent(cr):

openerp/modules/loading.py
@@ -269,6 +269,7 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
     try:
         if not openerp.modules.db.is_initialized(cr):
             _logger.info("init db")
+            update_module = True
             openerp.modules.db.initialize(cr)
             update_module = True # process auto-installed modules
             tools.config["init"]["all"] = 1
@@ -306,7 +307,8 @@ def load_modules(db, force_demo=False, status=None, update_module=False):
         # STEP 2: Mark other modules to be loaded/updated
         if update_module:
             modobj = registry['ir.module.module']
-            if ('base' in tools.config['init']) or ('base' in tools.config['update']):
+            init_update = tools.config['init'].keys() + tools.config['update'].keys()
+            if ('base' in init_update) or ('all' in init_update):
                 _logger.info('updating modules list')
                 modobj.update_list(cr, SUPERUSER_ID)
 