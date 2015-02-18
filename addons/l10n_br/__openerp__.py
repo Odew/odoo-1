b61512274de7076e5640e15a301d639dacda6737
ssh-odoo <ssh@openerp.com>
2015-02-18 19:24:19 +0530

    [imp]:Remove account(account.account.template) user_type='view'and link account(account.account.template) to field 'chart_template_id' or Rename account_collected_id -> refund_account_id, account_paid_id -> account_id, or Tax percentage should be multiplied by 100, rename type -> amount_type

--- addons/l10n_br/__openerp__.py
+++ addons/l10n_br/__openerp__.py
@@ -64,12 +64,11 @@ come with any additional paid permission for online use of 'private modules'.
     'version': '0.6',
     'depends': ['account'],
     'data': [
-                'data/account.account.type.csv',
-                'data/account.account.template.csv',
                 'data/account_chart_template.xml',
+                'data/account.account.template.csv',
                 'data/account_tax_template.xml',
                 'account_view.xml',
                 'l10n_br_wizard.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
