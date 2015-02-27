44f7b8c09b7feb5675142fedd271578f8cf30a39
Gaurav Panchal <gan@odoo.com>
2015-02-27 10:49:38 +0530

    [IMP]product_extended: Migrate into new API

--- addons/product_extended/__openerp__.py
+++ addons/product_extended/__openerp__.py
@@ -1,27 +1,9 @@
-##############################################################################
-#    
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2011 OpenERP S.A. (<http://www.openerp.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+
 {
     "name" : "Product extension to track sales and purchases",
     "version" : "1.0",
-    "author" : "OpenERP S.A.",
+    "author" : "Odoo S.A.",
     'website': 'https://www.odoo.com',
     "depends" : ["product", "purchase", "sale", "mrp", "stock_account"],
     "category" : "Generic Modules/Inventory Control",
@@ -32,7 +14,12 @@ Product extension. This module adds:
 """,
     "init_xml" : [],
     "demo_xml" : [],
-    "data" : ["product_extended_wizard.xml","product_extended_view.xml","mrp_view.xml", 'security/ir.model.access.csv'],
+    "data" : [
+        'wizard/product_extended_wizard.xml',
+        'views/product_extended_view.xml',
+        'views/mrp_view.xml',
+        'security/ir.model.access.csv'
+    ],
     "active": False,
     "installable": True
 }
