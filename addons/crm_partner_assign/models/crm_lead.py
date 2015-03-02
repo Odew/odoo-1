4f3b6914c07aa8d359a9ad8613e9364f00747fe6
Paramjit Singh Sahota <psa@openerp.com>
2015-03-02 12:30:29 +0530

    [MIG] Migrated 'crm_partner_assign' module into new api.

--- addons/crm_partner_assign/models/crm_lead.py
+++ addons/crm_partner_assign/models/crm_lead.py
@@ -1,32 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
-from openerp.osv import osv
-from openerp.tools.translate import _
+from openerp import _, api, models
 from openerp.exceptions import UserError
 
 
-class crm_lead(osv.osv):
+class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
+    @api.v7
     def get_interested_action(self, cr, uid, interested, context=None):
         try:
             model, action_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'crm_partner_assign', 'crm_lead_channel_interested_act')
@@ -38,16 +19,39 @@ class crm_lead(osv.osv):
         action['context'] = str(action_context)
         return action
 
+    @api.v8
+    def get_interested_action(self, interested):
+        try:
+            LeadChannelAct = self.env.ref('crm_partner_assign.crm_lead_channel_interested_act')
+        except ValueError:
+            raise UserError(_("The CRM Channel Interested Action is missing"))
+        action = self.env[LeadChannelAct._model].read([LeadChannelAct.id])
+        action_context = eval(action['context'])
+        action_context['interested'] = interested
+        action['context'] = str(action_context)
+        return action
+
+    @api.v7
     def case_interested(self, cr, uid, ids, context=None):
         return self.get_interested_action(cr, uid, True, context=context)
 
+    @api.v8
+    def case_interested(self):
+        return self.get_interested_action(True)
+
+    @api.v7
     def case_disinterested(self, cr, uid, ids, context=None):
         return self.get_interested_action(cr, uid, False, context=context)
 
+    @api.v8
+    def case_disinterested(self):
+        return self.get_interested_action(False)
+
+    @api.v7
     def assign_salesman_of_assigned_partner(self, cr, uid, ids, context=None):
         salesmans_leads = {}
         for lead in self.browse(cr, uid, ids, context=context):
-            if (lead.stage_id.probability > 0 and lead.stage_id.probability < 100) or lead.stage_id.sequence == 1: 
+            if (lead.stage_id.probability > 0 and lead.stage_id.probability < 100) or lead.stage_id.sequence == 1:
                 if lead.partner_assigned_id and lead.partner_assigned_id.user_id and lead.partner_assigned_id.user_id != lead.user_id:
                     salesman_id = lead.partner_assigned_id.user_id.id
                     if salesmans_leads.get(salesman_id):
@@ -57,3 +61,18 @@ class crm_lead(osv.osv):
         for salesman_id, lead_ids in salesmans_leads.items():
             salesteam_id = self.on_change_user(cr, uid, lead_ids, salesman_id, context=None)['value'].get('team_id')
             self.write(cr, uid, lead_ids, {'user_id': salesman_id, 'team_id': salesteam_id}, context=context)
+
+    @api.v8
+    def assign_salesman_of_assigned_partner(self):
+        salesmans_leads = {}
+        for lead in self:
+            if (lead.stage_id.probability > 0 and lead.stage_id.probability < 100) or lead.stage_id.sequence == 1:
+                if lead.partner_assigned_id and lead.partner_assigned_id.user_id and lead.partner_assigned_id.user_id != lead.user_id:
+                    salesman_id = lead.partner_assigned_id.user_id.id
+                    if salesmans_leads.get(salesman_id):
+                        salesmans_leads[salesman_id].append(lead.id)
+                    else:
+                        salesmans_leads[salesman_id] = [lead.id]
+        for salesman_id, lead_ids in salesmans_leads.items():
+            salesteam_id = self.on_change_user(lead_ids, salesman_id)['value'].get('team_id')
+            self.browse(lead_ids).write({'user_id': salesman_id, 'team_id': salesteam_id})
--- addons/crm_partner_assign/models/crm_partner_assign.py
+++ addons/crm_partner_assign/models/crm_partner_assign.py
@@ -1,145 +1,114 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 import random
-
+from openerp import api, fields, models
 from openerp.addons.base_geolocalize.models.res_partner import geo_find, geo_query_address
-from openerp.osv import osv
-from openerp.osv import fields
 
 
-class res_partner_grade(osv.osv):
+class ResPartnerGrade(models.Model):
     _order = 'sequence'
     _name = 'res.partner.grade'
-    _columns = {
-        'sequence': fields.integer('Sequence'),
-        'active': fields.boolean('Active'),
-        'name': fields.char('Grade Name'),
-        'partner_weight': fields.integer('Grade Weight',
-            help="Gives the probability to assign a lead to this partner. (0 means no assignation.)"),
-    }
-    _defaults = {
-        'active': lambda *args: 1,
-        'partner_weight':1
-    }
 
-class res_partner_activation(osv.osv):
+    sequence = fields.Integer()
+    active = fields.Boolean(default=True)
+    name = fields.Char(string='Grade Name')
+    partner_weight = fields.Integer(string='Grade Weight',
+                                    default=True,
+                                    help="Gives the probability to assign a lead to this partner. (0 means no assignation.)")
+
+
+class ResPartnerActivation(models.Model):
     _name = 'res.partner.activation'
     _order = 'sequence'
 
-    _columns = {
-        'sequence' : fields.integer('Sequence'),
-        'name' : fields.char('Name', required=True),
-    }
+    sequence = fields.Integer()
+    name = fields.Char(required=True)
 
 
-class res_partner(osv.osv):
+class ResPartner(models.Model):
     _inherit = "res.partner"
-    _columns = {
-        'partner_weight': fields.integer('Grade Weight',
-            help="Gives the probability to assign a lead to this partner. (0 means no assignation.)"),
-        'grade_id': fields.many2one('res.partner.grade', 'Grade'),
-        'activation' : fields.many2one('res.partner.activation', 'Activation', select=1),
-        'date_partnership' : fields.date('Partnership Date'),
-        'date_review' : fields.date('Latest Partner Review'),
-        'date_review_next' : fields.date('Next Partner Review'),
-        # customer implementation
-        'assigned_partner_id': fields.many2one(
-            'res.partner', 'Implemented by',
-        ),
-        'implemented_partner_ids': fields.one2many(
-            'res.partner', 'assigned_partner_id',
-            string='Implementation References',
-        ),
-    }
-    _defaults = {
-        'partner_weight': lambda *args: 0
-    }
-    
-    def onchange_grade_id(self, cr, uid, ids, grade_id, context=None):
-        res = {'value' :{'partner_weight':0}}
-        if grade_id:
-            partner_grade = self.pool.get('res.partner.grade').browse(cr, uid, grade_id)
-            res['value']['partner_weight'] = partner_grade.partner_weight
-        return res
+
+    partner_weight = fields.Integer(string='Grade Weight', default=0,
+        help="Gives the probability to assign a lead to this partner. (0 means no assignation.)")
+    grade_id = fields.Many2one('res.partner.grade', string='Grade')
+    activation = fields.Many2one('res.partner.activation', index=True)
+    date_partnership = fields.Date(string='Partnership Date')
+    date_review = fields.Date(string='Latest Partner Review')
+    date_review_next = fields.Date(string='Next Partner Review')
+    # customer implementation
+    assigned_partner_id = fields.Many2one('res.partner', string='Implemented by')
+    implemented_partner_ids = fields.One2many('res.partner', 'assigned_partner_id',
+                                              string='Implementation References')
+
+    @api.onchange('grade_id')
+    def onchange_grade_id(self):
+        self.partner_weight = self.grade_id.partner_weight
 
 
-class crm_lead(osv.osv):
+class CrmLead(models.Model):
     _inherit = "crm.lead"
-    _columns = {
-        'partner_latitude': fields.float('Geo Latitude', digits=(16, 5)),
-        'partner_longitude': fields.float('Geo Longitude', digits=(16, 5)),
-        'partner_assigned_id': fields.many2one('res.partner', 'Assigned Partner',track_visibility='onchange' , help="Partner this case has been forwarded/assigned to.", select=True),
-        'date_assign': fields.date('Assignation Date', help="Last date this case was forwarded/assigned to a partner"),
-    }
-    def _merge_data(self, cr, uid, ids, oldest, fields, context=None):
-        fields += ['partner_latitude', 'partner_longitude', 'partner_assigned_id', 'date_assign']
-        return super(crm_lead, self)._merge_data(cr, uid, ids, oldest, fields, context=context)
 
-    def onchange_assign_id(self, cr, uid, ids, partner_assigned_id, context=None):
-        """This function updates the "assignation date" automatically, when manually assign a partner in the geo assign tab
-        """
+    partner_latitude = fields.Float(string='Geo Latitude', digits=(16, 5))
+    partner_longitude = fields.Float(string='Geo Longitude', digits=(16, 5))
+    partner_assigned_id = fields.Many2one('res.partner', string='Assigned Partner',
+                                          track_visibility='onchange',
+                                          index=True,
+                                          help="Partner this case has been forwarded/assigned to.")
+    date_assign = fields.Date(string='Assignation Date',
+                              help="Last date this case was forwarded/assigned to a partner")
 
-        if not partner_assigned_id:
-            return {'value':{'date_assign': False}}
+    @api.model
+    def _merge_data(self, oldest, fields):
+        fields += ['partner_latitude', 'partner_longitude',
+                   'partner_assigned_id', 'date_assign']
+        return super(CrmLead, self)._merge_data(oldest, fields)
+
+    @api.onchange('partner_assigned_id')
+    def onchange_assign_id(self):
+        """This function updates the "assignation date" automatically,
+           when manually assign a partner in the geo assign tab"""
+        if not self.partner_assigned_id:
+            self.date_assign = False
         else:
-            partners = self.pool.get('res.partner').browse(cr, uid, [partner_assigned_id], context=context)
-            user_id = partners[0] and partners[0].user_id.id or False
-            return {'value':
-                        {'date_assign': fields.date.context_today(self,cr,uid,context=context),
-                         'user_id' : user_id}
-                   }
+            partners = self.env['res.partner'].browse(self.partner_assigned_id.id)
+            self.date_assign = fields.Date.context_today(self)
+            self.user_id = partners and partners.user_id.id or False
 
-    def action_assign_partner(self, cr, uid, ids, context=None):
-        return self.assign_partner(cr, uid, ids, partner_id=False, context=context)
+    @api.multi
+    def action_assign_partner(self):
+        return self.assign_partner(partner_id=False)
 
-    def assign_partner(self, cr, uid, ids, partner_id=False, context=None):
+    def assign_partner(self, partner_id=False):
         partner_ids = {}
         res = False
-        res_partner = self.pool.get('res.partner')
+        ResPartner = self.env['res.partner']
         if not partner_id:
-            partner_ids = self.search_geo_partner(cr, uid, ids, context=context)
-        for lead in self.browse(cr, uid, ids, context=context):
+            partner_ids = self.search_geo_partner()
+        for lead in self:
             if not partner_id:
                 partner_id = partner_ids.get(lead.id, False)
             if not partner_id:
                 continue
-            self.assign_geo_localize(cr, uid, [lead.id], lead.partner_latitude, lead.partner_longitude, context=context)
-            partner = res_partner.browse(cr, uid, partner_id, context=context)
+            self.assign_geo_localize(lead.partner_latitude, lead.partner_longitude)
+            partner = ResPartner.browse(partner_id)
             if partner.user_id:
                 salesteam_id = partner.team_id and partner.team_id.id or False
-                for lead_id in ids:
-                    self.allocate_salesman(cr, uid, [lead_id], [partner.user_id.id], team_id=salesteam_id, context=context)
-            self.write(cr, uid, [lead.id], {'date_assign': fields.date.context_today(self,cr,uid,context=context), 'partner_assigned_id': partner_id}, context=context)
+                for lead_id in self.ids:
+                    self.allocate_salesman(
+                        [lead_id], [partner.user_id.id], team_id=salesteam_id)
+
+            self.browse(lead.id).write({
+                'date_assign': fields.Date.context_today(self),
+                'partner_assigned_id': partner_id})
         return res
 
-    def assign_geo_localize(self, cr, uid, ids, latitude=False, longitude=False, context=None):
+    def assign_geo_localize(self, latitude=False, longitude=False):
         if latitude and longitude:
-            self.write(cr, uid, ids, {
+            self.write({
                 'partner_latitude': latitude,
                 'partner_longitude': longitude
-            }, context=context)
-            return True
-        # Don't pass context to browse()! We need country name in english below
-        for lead in self.browse(cr, uid, ids):
+            })
+        for lead in self:
             if lead.partner_latitude and lead.partner_longitude:
                 continue
             if lead.country_id:
@@ -149,77 +118,86 @@ class crm_lead(osv.osv):
                                                     state=lead.state_id.name,
                                                     country=lead.country_id.name))
                 if result:
-                    self.write(cr, uid, [lead.id], {
+                    self.browse(lead.id).write({
                         'partner_latitude': result[0],
                         'partner_longitude': result[1]
-                    }, context=context)
-        return True
+                    })
 
-    def search_geo_partner(self, cr, uid, ids, context=None):
-        res_partner = self.pool.get('res.partner')
+    def search_geo_partner(self):
+        ResPartner = self.env['res.partner']
         res_partner_ids = {}
-        self.assign_geo_localize(cr, uid, ids, context=context)
-        for lead in self.browse(cr, uid, ids, context=context):
-            partner_ids = []
+        self.assign_geo_localize()
+        for lead in self:
             if not lead.country_id:
                 continue
             latitude = lead.partner_latitude
             longitude = lead.partner_longitude
             if latitude and longitude:
                 # 1. first way: in the same country, small area
-                partner_ids = res_partner.search(cr, uid, [
+                Partners = ResPartner.search([
                     ('partner_weight', '>', 0),
-                    ('partner_latitude', '>', latitude - 2), ('partner_latitude', '<', latitude + 2),
-                    ('partner_longitude', '>', longitude - 1.5), ('partner_longitude', '<', longitude + 1.5),
+                    ('partner_latitude', '>', latitude - 2),
+                    ('partner_latitude', '<', latitude + 2),
+                    ('partner_longitude', '>', longitude - 1.5),
+                    ('partner_longitude', '<', longitude + 1.5),
                     ('country_id', '=', lead.country_id.id),
-                ], context=context)
+                ])
 
                 # 2. second way: in the same country, big area
-                if not partner_ids:
-                    partner_ids = res_partner.search(cr, uid, [
+                if not Partners:
+                    Partners = ResPartner.search([
                         ('partner_weight', '>', 0),
-                        ('partner_latitude', '>', latitude - 4), ('partner_latitude', '<', latitude + 4),
-                        ('partner_longitude', '>', longitude - 3), ('partner_longitude', '<' , longitude + 3),
+                        ('partner_latitude', '>', latitude - 4),
+                        ('partner_latitude', '<', latitude + 4),
+                        ('partner_longitude', '>', longitude - 3),
+                        ('partner_longitude', '<', longitude + 3),
                         ('country_id', '=', lead.country_id.id),
-                    ], context=context)
+                    ])
 
                 # 3. third way: in the same country, extra large area
-                if not partner_ids:
-                    partner_ids = res_partner.search(cr, uid, [
-                        ('partner_weight','>', 0),
-                        ('partner_latitude','>', latitude - 8), ('partner_latitude','<', latitude + 8),
-                        ('partner_longitude','>', longitude - 8), ('partner_longitude','<', longitude + 8),
+                if not Partners:
+                    Partners = ResPartner.search([
+                        ('partner_weight', '>', 0),
+                        ('partner_latitude', '>', latitude - 8),
+                        ('partner_latitude', '<', latitude + 8),
+                        ('partner_longitude', '>', longitude - 8),
+                        ('partner_longitude', '<', longitude + 8),
                         ('country_id', '=', lead.country_id.id),
-                    ], context=context)
+                    ])
 
                 # 5. fifth way: anywhere in same country
-                if not partner_ids:
-                    # still haven't found any, let's take all partners in the country!
-                    partner_ids = res_partner.search(cr, uid, [
+                if not Partners:
+                    # still haven't found any, let's take all partners in the
+                    # country!
+                    Partners = ResPartner.search([
                         ('partner_weight', '>', 0),
                         ('country_id', '=', lead.country_id.id),
-                    ], context=context)
+                    ])
 
-                # 6. sixth way: closest partner whatsoever, just to have at least one result
-                if not partner_ids:
-                    # warning: point() type takes (longitude, latitude) as parameters in this order!
-                    cr.execute("""SELECT id, distance
+                # 6. sixth way: closest partner whatsoever, just to have at
+                # least one result
+                if not Partners:
+                    # warning: point() type takes (longitude, latitude) as
+                    # parameters in this order!
+                    self.env.cr.execute("""SELECT id, distance
                                   FROM  (select id, (point(partner_longitude, partner_latitude) <-> point(%s,%s)) AS distance FROM res_partner
                                   WHERE partner_longitude is not null
                                         AND partner_latitude is not null
                                         AND partner_weight > 0) AS d
                                   ORDER BY distance LIMIT 1""", (longitude, latitude))
-                    res = cr.dictfetchone()
+                    res = self.env.cr.dictfetchone()
                     if res:
-                        partner_ids.append(res['id'])
+                        Partners = Partners + ResPartner.browse(res['id'])
 
                 total_weight = 0
                 toassign = []
-                for partner in res_partner.browse(cr, uid, partner_ids, context=context):
+                for partner in Partners:
                     total_weight += partner.partner_weight
-                    toassign.append( (partner.id, total_weight) )
+                    toassign.append((partner.id, total_weight))
 
-                random.shuffle(toassign) # avoid always giving the leads to the first ones in db natural order!
+                # avoid always giving the leads to the first ones in db natural
+                # order!
+                random.shuffle(toassign)
                 nearest_weight = random.randint(0, total_weight)
                 for partner_id, weight in toassign:
                     if nearest_weight <= weight:
