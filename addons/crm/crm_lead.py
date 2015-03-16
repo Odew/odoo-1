tac-odoo/odoo

[IMP]Calendar,crm,fleet,hr,hr_recruitment,mass_mailing,note,project/project_issue,website_blog,website_event,website_forum: making all tag unique, case and accent insensitive in this modules

addons/crm/crm_lead.py
@@ -1094,6 +1094,7 @@ class crm_lead_tag(osv.Model):
         'name': fields.char('Name', required=True, translate=True),
         'team_id': fields.many2one('crm.team', 'Sales Team'),
     }
+    _constraints = [(osv.Model._check_unique_accent, _('Error! Tag name already exist.'), ['name'])]
 
 
 class crm_lost_reason(osv.Model):

addons/fleet/fleet.py
@@ -97,6 +97,7 @@ class fleet_vehicle_tag(osv.Model):
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
     }
+    _constraints = [(osv.Model._check_unique_accent, _('Error! Tag name already exist.'), ['name'])]
 
 class fleet_vehicle_state(osv.Model):
     _name = 'fleet.vehicle.state'

addons/hr_recruitment/hr_recruitment.py
@@ -623,3 +623,4 @@ class applicant_category(osv.osv):
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
     }
+    _constraints = [(osv.osv._check_unique_accent, _('Error! Tag name already exist.'), ['name'])]

addons/note/note.py
@@ -22,6 +22,7 @@
 from openerp import SUPERUSER_ID
 from openerp.osv import osv, fields
 from openerp.tools import html2plaintext
+from openerp.tools.translate import _
 
 class note_stage(osv.osv):
     """ Category of Note """
@@ -46,6 +47,7 @@ class note_tag(osv.osv):
     _columns = {
         'name' : fields.char('Tag Name', required=True),
     }
+    _constraints = [(osv.osv._check_unique_accent, _('Error! Tag name already exist.'), ['name'])]
 
 class note_note(osv.osv):
     """ Note """

addons/project/project.py
@@ -1430,4 +1430,5 @@ class project_category(osv.osv):
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
     }
+    _constraints = [(osv.osv._check_unique_accent, _('Error! Tag name already exist.'), ['name'])]
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

addons/website_blog/models/website_blog.py
@@ -61,7 +61,7 @@ class BlogTag(osv.Model):
             'blog.post', string='Posts',
         ),
     }
-
+    _constraints = [(osv.Model._check_unique_accent, _('Error! Tag name already exist.'), ['name'])]
 
 class BlogPost(osv.Model):
     _name = "blog.post"

Odew/odoo-1

addons/crm/crm_lead.py
@@ -1106,6 +1106,7 @@ class crm_lead_tag(osv.Model):
         'name': fields.char('Name', required=True, translate=True),
         'team_id': fields.many2one('crm.team', 'Sales Team'),
     }
+    _constraints = [(osv.Model._check_unique_accent, _('Error! Tag name already exists.'), ['name'])]
 
 
 class crm_lost_reason(osv.Model):

addons/fleet/fleet.py
@@ -97,6 +97,7 @@ class fleet_vehicle_tag(osv.Model):
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
     }
+    _constraints = [(osv.Model._check_unique_accent, _('Error! Tag name already exists.'), ['name'])]
 
 class fleet_vehicle_state(osv.Model):
     _name = 'fleet.vehicle.state'

addons/hr_recruitment/hr_recruitment.py
@@ -700,6 +700,7 @@ class applicant_category(osv.osv):
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
     }
+    _constraints = [(osv.osv._check_unique_accent, _('Error! Tag name already exists.'), ['name'])]
 
 
 class hr_employee(osv.Model):

addons/note/note.py
@@ -22,6 +22,7 @@
 from openerp import SUPERUSER_ID
 from openerp.osv import osv, fields
 from openerp.tools import html2plaintext
+from openerp.tools.translate import _
 
 class note_stage(osv.osv):
     """ Category of Note """
@@ -46,6 +47,7 @@ class note_tag(osv.osv):
     _columns = {
         'name' : fields.char('Tag Name', required=True),
     }
+    _constraints = [(osv.osv._check_unique_accent, _('Error! Tag name already exists.'), ['name'])]
 
 class note_note(osv.osv):
     """ Note """

addons/project/project.py
@@ -1414,3 +1414,4 @@ class project_category(osv.osv):
     _columns = {
         'name': fields.char('Name', required=True, translate=True),
     }
+    _constraints = [(osv.osv._check_unique_accent, _('Error! Tag name already exists.'), ['name'])]

addons/website_blog/models/website_blog.py
@@ -59,7 +59,7 @@ class BlogTag(osv.Model):
             'blog.post', string='Posts',
         ),
     }
-
+    _constraints = [(osv.Model._check_unique_accent, _('Error! Tag name already exists.'), ['name'])]
 
 class BlogPost(osv.Model):
     _name = "blog.post"