DonCDS/odoo

[NEW API] Convert product_email_template module in new api.

addons/product_email_template/models/invoice.py
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv
+from openerp import api, models
 
 
-class account_invoice(osv.Model):
+class account_invoice(models.Model):
     _inherit = 'account.invoice'
 
-    def invoice_validate_send_email(self, cr, uid, ids, context=None):
-        Composer = self.pool['mail.compose.message']
-        for invoice in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    def invoice_validate_send_email(self):
+        Composer = self.env['mail.compose.message']
+        for invoice in self:
             # send template only on customer invoice
             if invoice.type != 'out_invoice':
                 continue
             # subscribe the partner to the invoice
             if invoice.partner_id not in invoice.message_follower_ids:
-                self.message_subscribe(cr, uid, [invoice.id], [invoice.partner_id.id], context=context)
+                invoice.message_subscribe([invoice.partner_id.id])
             for line in invoice.invoice_line:
                 if line.product_id.email_template_id:
                     # CLEANME: should define and use a clean API: message_post with a template
-                    composer_id = Composer.create(cr, uid, {
+                    composer = Composer.create({
                         'model': 'account.invoice',
                         'res_id': invoice.id,
                         'template_id': line.product_id.email_template_id.id,
-                        'composition_mode': 'comment',
-                    }, context=context)
-                    template_values = Composer.onchange_template_id(
-                        cr, uid, composer_id, line.product_id.email_template_id.id, 'comment', 'account.invoice', invoice.id
-                    )['value']
+                        'composition_mode': 'comment'})
+                    template_values = composer.onchange_template_id(line.product_id.email_template_id.id, 'comment', 'account.invoice', invoice.id)['value']
                     template_values['attachment_ids'] = [(4, id) for id in template_values.get('attachment_ids', [])]
-                    Composer.write(cr, uid, [composer_id], template_values, context=context)
-                    Composer.send_mail(cr, uid, [composer_id], context=context)
+                    composer.write(template_values)
+                    composer.send_mail()
         return True
 
-    def invoice_validate(self, cr, uid, ids, context=None):
-        res = super(account_invoice, self).invoice_validate(cr, uid, ids, context=context)
-        self.invoice_validate_send_email(cr, uid, ids, context=context)
+    @api.multi
+    def invoice_validate(self):
+        res = super(account_invoice, self).invoice_validate()
+        self.invoice_validate_send_email()
         return res

Odew/odoo-1

addons/product_email_template/models/invoice.py
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv
+from openerp import api, models
 
 
-class account_invoice(osv.Model):
+class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    def invoice_validate_send_email(self, cr, uid, ids, context=None):
-        Composer = self.pool['mail.compose.message']
-        for invoice in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    def invoice_validate_send_email(self):
+        Composer = self.env['mail.compose.message']
+        for invoice in self:
             # send template only on customer invoice
             if invoice.type != 'out_invoice':
                 continue
             # subscribe the partner to the invoice
             if invoice.partner_id not in invoice.message_follower_ids:
-                self.message_subscribe(cr, uid, [invoice.id], [invoice.partner_id.id], context=context)
+                invoice.message_subscribe([invoice.partner_id.id])
             for line in invoice.invoice_line:
                 if line.product_id.email_template_id:
                     # CLEANME: should define and use a clean API: message_post with a template
-                    composer_id = Composer.create(cr, uid, {
+                    composer = Composer.create({
                         'model': 'account.invoice',
                         'res_id': invoice.id,
                         'template_id': line.product_id.email_template_id.id,
-                        'composition_mode': 'comment',
-                    }, context=context)
-                    template_values = Composer.onchange_template_id(
-                        cr, uid, composer_id, line.product_id.email_template_id.id, 'comment', 'account.invoice', invoice.id
-                    )['value']
+                        'composition_mode': 'comment'})
+                    template_values = composer.onchange_template_id(line.product_id.email_template_id.id, 'comment', 'account.invoice', invoice.id)['value']
                     template_values['attachment_ids'] = [(4, id) for id in template_values.get('attachment_ids', [])]
-                    Composer.write(cr, uid, [composer_id], template_values, context=context)
-                    Composer.send_mail(cr, uid, [composer_id], context=context)
+                    composer.write(template_values)
+                    composer.send_mail()
         return True
 
-    def invoice_validate(self, cr, uid, ids, context=None):
-        res = super(account_invoice, self).invoice_validate(cr, uid, ids, context=context)
-        self.invoice_validate_send_email(cr, uid, ids, context=context)
+    @api.multi
+    def invoice_validate(self):
+        res = super(AccountInvoice, self).invoice_validate()
+        self.invoice_validate_send_email()
         return res