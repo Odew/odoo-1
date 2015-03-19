serkia/odoo

[ADD] Website_Sale_Layout: Added a module to implement a layout like sale layout in web quotation, when the module is installed it will modify the pricing layout according to sections defined by user while creating a quotation.

addons/website_sale_layout/__openerp__.py
@@ -1,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website Sale Layout',
+    'version': '1.0',
+    'summary': 'Web Quotation Layout, page-break, subtotals, separators',
+    'description': """
+        With this module you can personalize the web quotation with separators, page-breaks or subtotals.
+    """,
+    'author': 'OpenERP SA',
+    'website': 'http://www.odoo.com',
+    'depends': ['sale_layout','website_quote'],
+    'category': 'Website',
+    'data': ['views/website_sale_layout.xml',
+             'views/website_sale_layout_template.xml'],
+    'installable': True,
+}

Odew/odoo-1

addons/website_quote_layout/__openerp__.py
@@ -1,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website Quote Layout',
+    'version': '1.0',
+    'summary': 'Web Quotation Layout, page-break, subtotals, separators',
+    'description': """
+        With this module you can personalize the web quotation with separators, page-breaks or subtotals.
+    """,
+    'author': 'Odoo SA',
+    'website': 'http://www.odoo.com',
+    'depends': ['sale_layout','website_quote'],
+    'category': 'Website',
+    'data': ['views/website_quote_layout_template.xml',
+             'views/website_quote_layout.xml'],
+    'installable': True,
+}