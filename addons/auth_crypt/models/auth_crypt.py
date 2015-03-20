DonCDS/odoo

[MIG]auth_crypt:Migrate to new api

addons/auth_crypt/auth_crypt.py
@@ -3,7 +3,7 @@ import logging
 from passlib.context import CryptContext
 
 import openerp
-from openerp.osv import fields, osv
+from openerp import models, api, fields
 
 _logger = logging.getLogger(__name__)
 
@@ -18,9 +18,10 @@ default_crypt_context = CryptContext(
     deprecated=['md5_crypt'],
 )
 
-class res_users(osv.osv):
+class res_users(models.Model):
     _inherit = "res.users"
 
+    #need to migrate
     def init(self, cr):
         _logger.info("Hashing passwords, may be slow for databases with many users...")
         cr.execute("SELECT id, password FROM res_users"
@@ -28,62 +29,66 @@ class res_users(osv.osv):
                    "   AND password != ''")
         for uid, pwd in cr.fetchall():
             self._set_password(cr, openerp.SUPERUSER_ID, uid, pwd)
+    
+    @api.one
+    def set_pw(self):
+        if self.password:
+            self._set_password(self.password)
+            self.invalidate_cache()
 
-    def set_pw(self, cr, uid, id, name, value, args, context):
-        if value:
-            self._set_password(cr, uid, id, value, context=context)
-            self.invalidate_cache(cr, uid, context=context)
+    @api.one
+    def get_pw(self):
+        self._cr.execute('select id, password from res_users where id in %s', (self._ids))
+        return dict(self._cr.fetchall())
 
-    def get_pw( self, cr, uid, ids, name, args, context ):
-        cr.execute('select id, password from res_users where id in %s', (tuple(map(int, ids)),))
-        return dict(cr.fetchall())
 
-    _columns = {
-        'password': fields.function(get_pw, fnct_inv=set_pw, type='char', string='Password', invisible=True, store=True),
-        'password_crypt': fields.char(string='Encrypted Password', invisible=True, copy=False),
-    }
+    password = fields.Char(compute='get_pw', inverse='set_pw', string='Password', invisible=True, store=True)
+    password_crypt = fields.Char(string='Encrypted Password', invisible=True, copy=False)
 
-    def check_credentials(self, cr, uid, password):
+    @api.model
+    def check_credentials(self, password):
         # convert to base_crypt if needed
-        cr.execute('SELECT password, password_crypt FROM res_users WHERE id=%s AND active', (uid,))
+        self._cr.execute('SELECT password, password_crypt FROM res_users WHERE id=%s AND active', (self._uid,))
         encrypted = None
-        if cr.rowcount:
-            stored, encrypted = cr.fetchone()
+        if self._cr.rowcount:
+            stored, encrypted = self._cr.fetchone()
             if stored and not encrypted:
-                self._set_password(cr, uid, uid, stored)
-                self.invalidate_cache(cr, uid)
+                self._set_password(stored)
+                self.invalidate_cache()
         try:
-            return super(res_users, self).check_credentials(cr, uid, password)
+            return super(res_users, self).check_credentials(password)
         except openerp.exceptions.AccessDenied:
             if encrypted:
-                valid_pass, replacement = self._crypt_context(cr, uid, uid)\
+                valid_pass, replacement = self._crypt_context()\
                         .verify_and_update(password, encrypted)
                 if replacement is not None:
-                    self._set_encrypted_password(cr, uid, uid, replacement)
+                    self._set_encrypted_password(replacement)
                 if valid_pass:
                     return
-
             raise
 
-    def _set_password(self, cr, uid, id, password, context=None):
+    @api.one
+    def _set_password(self, password):
         """ Encrypts then stores the provided plaintext password for the user
         ``id``
         """
-        encrypted = self._crypt_context(cr, uid, id, context=context).encrypt(password)
-        self._set_encrypted_password(cr, uid, id, encrypted, context=context)
+        encrypted = self._crypt_context().encrypt(password)
+        self._set_encrypted_password(encrypted)
 
-    def _set_encrypted_password(self, cr, uid, id, encrypted, context=None):
+    @api.multi
+    def _set_encrypted_password(self, encrypted):
         """ Store the provided encrypted password to the database, and clears
         any plaintext password
 
         :param uid: id of the current user
         :param id: id of the user on which the password should be set
         """
-        cr.execute(
+        self._cr.execute(
             "UPDATE res_users SET password='', password_crypt=%s WHERE id=%s",
-            (encrypted, id))
+            (encrypted, self.id))
 
-    def _crypt_context(self, cr, uid, id, context=None):
+    @api.model
+    def _crypt_context(self):
         """ Passlib CryptContext instance used to encrypt and verify
         passwords. Can be overridden if technical, legal or political matters
         require different kdfs than the provided default.

Odew/odoo-1

addons/auth_crypt/models/auth_crypt.py
@@ -1,9 +1,10 @@
-import logging
+# -*- coding: utf-8 -*-
 
+import logging
 from passlib.context import CryptContext
 
 import openerp
-from openerp.osv import fields, osv
+from openerp import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -18,7 +19,8 @@ default_crypt_context = CryptContext(
     deprecated=['md5_crypt'],
 )
 
-class res_users(osv.osv):
+
+class ResUsers(models.Model):
     _inherit = "res.users"
 
     def init(self, cr):
@@ -29,61 +31,61 @@ class res_users(osv.osv):
         for uid, pwd in cr.fetchall():
             self._set_password(cr, openerp.SUPERUSER_ID, uid, pwd)
 
-    def set_pw(self, cr, uid, id, name, value, args, context):
-        if value:
-            self._set_password(cr, uid, id, value, context=context)
-            self.invalidate_cache(cr, uid, context=context)
+    @api.one
+    def set_pw(self):
+        if self.password:
+            self._set_password(self.password)
+            self.env.invalidate_all()
 
-    def get_pw( self, cr, uid, ids, name, args, context ):
-        cr.execute('select id, password from res_users where id in %s', (tuple(map(int, ids)),))
-        return dict(cr.fetchall())
+    @api.one
+    @api.depends('password_crypt')
+    def get_pw(self):
+        self.env.cr.execute('select password from res_users where id = %s', (self.id,))
+        self.password = self.env.cr.fetchone()[0]
 
-    _columns = {
-        'password': fields.function(get_pw, fnct_inv=set_pw, type='char', string='Password', invisible=True, store=True),
-        'password_crypt': fields.char(string='Encrypted Password', invisible=True, copy=False),
-    }
+    password = fields.Char(compute='get_pw', inverse='set_pw', invisible=True, store=True)
+    password_crypt = fields.Char(string='Encrypted Password', invisible=True, copy=False)
 
-    def check_credentials(self, cr, uid, password):
+    @api.model
+    def check_credentials(self, password):
         # convert to base_crypt if needed
-        cr.execute('SELECT password, password_crypt FROM res_users WHERE id=%s AND active', (uid,))
+        self.env.cr.execute('SELECT password, password_crypt FROM res_users WHERE id=%s AND active', (self.env.uid,))
         encrypted = None
-        if cr.rowcount:
-            stored, encrypted = cr.fetchone()
+        user = self.env.user
+        if self.env.cr.rowcount:
+            stored, encrypted = self.env.cr.fetchone()
             if stored and not encrypted:
-                self._set_password(cr, uid, uid, stored)
-                self.invalidate_cache(cr, uid)
+                user._set_password(stored)
+                self.env.invalidate_all()
         try:
-            return super(res_users, self).check_credentials(cr, uid, password)
+            return super(ResUsers, self).check_credentials(password)
         except openerp.exceptions.AccessDenied:
             if encrypted:
-                valid_pass, replacement = self._crypt_context(cr, uid, uid)\
+                valid_pass, replacement = user._crypt_context()\
                         .verify_and_update(password, encrypted)
                 if replacement is not None:
-                    self._set_encrypted_password(cr, uid, uid, replacement)
+                    user._set_encrypted_password(replacement)
                 if valid_pass:
                     return
-
             raise
 
-    def _set_password(self, cr, uid, id, password, context=None):
+    @api.one
+    def _set_password(self, password):
         """ Encrypts then stores the provided plaintext password for the user
         ``id``
         """
-        encrypted = self._crypt_context(cr, uid, id, context=context).encrypt(password)
-        self._set_encrypted_password(cr, uid, id, encrypted, context=context)
+        encrypted = self._crypt_context().encrypt(password)
+        self._set_encrypted_password(encrypted)
 
-    def _set_encrypted_password(self, cr, uid, id, encrypted, context=None):
+    def _set_encrypted_password(self, encrypted):
         """ Store the provided encrypted password to the database, and clears
         any plaintext password
-
-        :param uid: id of the current user
-        :param id: id of the user on which the password should be set
         """
-        cr.execute(
+        self.env.cr.execute(
             "UPDATE res_users SET password='', password_crypt=%s WHERE id=%s",
-            (encrypted, id))
+            (encrypted, self.id))
 
-    def _crypt_context(self, cr, uid, id, context=None):
+    def _crypt_context(self):
         """ Passlib CryptContext instance used to encrypt and verify
         passwords. Can be overridden if technical, legal or political matters
         require different kdfs than the provided default.