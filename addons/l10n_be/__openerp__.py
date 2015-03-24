a630dbbaaeed73de377a399be038ca000c7f200e
pvy <pvy@odoo.com>
2015-03-24 15:36:36 +0100

    [FIX] Sale : fixed tests

--- addons/l10n_be/__openerp__.py
+++ addons/l10n_be/__openerp__.py
@@ -80,7 +80,6 @@ Wizards provided by this module:
         'report_vat_statement.xml',
     ],
     'demo': [
-        '../account/demo/account_journals_demo.yml',
         '../account/demo/account_bank_statement.xml',
         '../account/demo/account_invoice_demo.xml',
         '../account/demo/account_invoice_demo.yml',
--- addons/sale/tests/test_sale_to_invoice.py
+++ addons/sale/tests/test_sale_to_invoice.py
@@ -34,13 +34,29 @@ class TestSale(TestMail):
         import time
         # Usefull models
         IrModelData = self.env['ir.model.data']
+        partner_obj = self.env['res.partner']
+        journal_obj = self.env['account.journal']
+        account_obj = self.env['account.account']
         # Usefull record id
         group_id = IrModelData.xmlid_to_res_id('account.group_account_invoice') or False
         product_id = IrModelData.xmlid_to_res_id('product.product_category_3') or False
-        account_id = IrModelData.xmlid_to_res_id('account.cash') or False
         company_id = IrModelData.xmlid_to_res_id('base.main_company') or False
-        journal_id = IrModelData.xmlid_to_res_id('account.bank_journal') or False
+        journal_id = journal_obj.search([('name', '=', 'Bank Bank Account (test)')])[0]
+        account_id = journal_id.default_credit_account_id.id
         date = time.strftime("%Y/%m/%d")
+
+        # Usefull accounts
+        user_type_id = IrModelData.xmlid_to_res_id('account.data_account_type_revenue')
+        account_rev_id = account_obj.create({'code': 'X2020', 'name': 'Sales - Test Sales Account', 'user_type': user_type_id})
+        user_type_id = IrModelData.xmlid_to_res_id('account.data_account_type_receivable')
+        account_recv_id = account_obj.create({'code': 'X1012', 'name': 'Sales - Test Reicv Account', 'user_type': user_type_id})
+
+        # Add account to product
+        product_template_id = self.env.ref('sale.advance_product_0').product_tmpl_id
+        product_template_id.write({'property_account_income': account_rev_id})
+
+        # Create Sale Journal
+        journal_obj.create({'name': 'Sale Journal - Test', 'code': 'STSJ', 'type': 'sale', 'company_id': company_id})
 
         # In order to test, I create new user and applied Invoicing & Payments group.
         user = self.env['res.users'].create({
@@ -50,9 +66,12 @@ class TestSale(TestMail):
             'groups_id': [(6, 0, [group_id])]})
         assert user, "User will not created."
         # I create partner for sale order.
-        partner = self.env['res.partner'].create({
+        partner = partner_obj.create({
             'name': 'Test Customer',
-            'email': 'testcustomer@test.com'})
+            'email': 'testcustomer@test.com',
+            'property_account_receivable': account_recv_id,
+        })
+        
         # In order to test I create sale order and confirmed it.
         order = self.env['sale.order'].create({
             'partner_id': partner.id,
@@ -75,7 +94,7 @@ class TestSale(TestMail):
             'account_id': account_id,
             'amount': 75.0,
             'company_id': company_id,
-            'journal_id': journal_id,
+            'journal_id': journal_id.id,
             'partner_id': partner.id,
             'date': date,
             'voucher_type': 'sale',
