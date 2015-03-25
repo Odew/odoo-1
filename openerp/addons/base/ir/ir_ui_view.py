a63f7fcc872936390fbb76f6875c5125ee2c03f4
Raphael Collet <rco@openerp.com>
2015-03-25 17:21:47 +0100

    [IMP] adapt all calls to ormcache() to its new API
    This fixes a bug on some methods.  Because `ormcache` uses the decorator
    `decorator`, the `context` argument is passed positionally to the decorated
    lookup method, instead of being passed as a keyword argument.  As a
    consequence, the context dictionary is included in the cache key, which makes
    the cache lookup fail all the time.
    
    Using the new API of `ormcache` gracefully fixes this issue.

--- openerp/addons/base/ir/ir_ui_view.py
+++ openerp/addons/base/ir/ir_ui_view.py
@@ -947,7 +947,7 @@ class view(osv.osv):
             view_id = self.get_view_id(cr, uid, xml_id, context=context)
         return self._read_template(cr, uid, view_id, context=context)
 
-    @tools.ormcache(skiparg=3)
+    @tools.ormcache('xml_id')
     def get_view_id(self, cr, uid, xml_id, context=None):
         return self.pool['ir.model.data'].xmlid_to_res_id(cr, uid, xml_id, raise_if_not_found=True)
 
