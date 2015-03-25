956395944fc352f510eb13a8c5f5cca1dfad2fa7
fka-odoo <fka@openerp.com>
2015-03-25 11:08:34 +0530

    [IMP] Add company_registry field in res.partner and improve the vat usability
    [IMP] remove icon on the check validity button
    
    [IMP] remove string and extra space

--- addons/base_vat/base_vat.py
+++ addons/base_vat/base_vat.py
@@ -136,9 +136,6 @@ class res_partner(osv.osv):
                 return False
         return True
 
-    def vat_change(self, cr, uid, ids, value, context=None):
-        return {'value': {'vat_subjected': bool(value)}}
-
     def _commercial_fields(self, cr, uid, context=None):
         return super(res_partner, self)._commercial_fields(cr, uid, context=context) + ['vat_subjected']
 
